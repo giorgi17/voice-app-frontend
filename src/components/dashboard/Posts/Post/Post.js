6ef82317c3deb28b3f4c4799ae8b48ba42ba797e
giorgi17 <giorgi199817@gmail.com>
2020-04-27 20:09:51 +0400

    Fetching posts and displaying, audio player

--- src/components/dashboard/Posts/Post/Post.js
+++ src/components/dashboard/Posts/Post/Post.js
@@ -1,52 +1,74 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Post.css';
+import Player from '../../Menu/RecordVoice/Player/Player';
 
-const Post = (props) => (
-    <div className="single-post">
-        <div className="user-post-image-wrapper">
-            <div className="user-post-play-and-time">
-                <span className="material-icons user-post-play-button">
-                    play_circle_outline
-                </span>
-                <span className="user-post-video-time">00:17:32</span>
-            </div>
-            <img src="https://images.fineartamerica.com/images/artworkimages/mediumlarge/3/backgroung-concept-for-travel-in-japan-image-anek-suwannaphoom.jpg"></img>
-        </div>
-
-        <div className="user-post-profile-image-wrapper">
-            <img src="https://images.ladbible.com/thumbnail?type=jpeg&url=http://beta.ems.ladbiblegroup.com/s3/content/1bb3cdf35dc6d4a97a9891fef90de232.png&quality=70&width=720" />
-        </div>
-
-        <strong className="user-post-username">Giorgi khachidze</strong>
-
-        <div className="user-post-viewer-options">
-            <span className="material-icons like">
-                thumb_up
-            </span>
-            <span className="material-icons dislike">
-                thumb_down
-            </span>
-            <span className="material-icons comment">
-                mode_comment
-            </span>
-            <span className="material-icons share">
-                share
-            </span>
-        </div>
-
-        <div className="user-post-info">
-            <span className="user-post-info-likes">1 Likes</span>
-            <span className="user-post-info-dislikes">17 Dislikes</span>
-            <span className="user-post-info-comments">15 Comments</span>
-            <span className="user-post-info-views">502 Views</span>
-        </div>
-        <hr></hr>
-
-        <div className="user-post-description">
-            <span>Esse reprehenderit adipisicing sunt magna. Incididunt laboris dolore nisi dolore minim aliqua est non nostrud eiusmod minim culpa. Ea ullamco consectetur proident consequat cupidatat laboris officia velit. Do consequat id irure laboris proident. Qui ad velit eu cillum quis irure incididunt reprehenderit minim. Lorem quis aute dolore enim fugiat sint consequat adipisicing.
-a ullamcnim ea id est minim excepteur magna aute non.</span>
-        </div>
-    </div>
-);
+class Post extends Component {
+    constructor() {
+        super();
+        this.playerRef = React.createRef();
+        this.state = {
+            player: null
+        }
+    }
+    
+    playAudio = () => {
+        this.playerRef.current.style.display = 'none';
+        this.setState({player: (
+            <div id="user-post-player-container">
+                <Player audioFile={this.props.audio} />
+            </div>)});
+    }
+    
+    render() {
+        return (
+            <div className="single-post">
+                <div className="user-post-image-wrapper">
+                    <div className="user-post-play-and-time" ref={this.playerRef}>
+                        <span className="material-icons user-post-play-button" onClick={this.playAudio}>
+                            play_circle_outline
+                        </span>
+                        <span className="user-post-video-time">{this.props.audio_duration}</span>
+                    </div>
+                    <img src={this.props.picture}></img>
+                </div>
+    
+                <div className="user-post-profile-image-wrapper">
+                    <img src={this.props.profile_picture} />
+                </div>
+    
+                <strong className="user-post-username">{this.props.user_name}</strong>
+    
+                <div className="user-post-viewer-options">
+                    <span className="material-icons like">
+                        thumb_up
+                    </span>
+                    <span className="material-icons dislike">
+                        thumb_down
+                    </span>
+                    <span className="material-icons comment">
+                        mode_comment
+                    </span>
+                    <span className="material-icons share">
+                        share
+                    </span>
+                </div>
+    
+                 {this.state.player}
+    
+                <div className="user-post-info">
+                    <span className="user-post-info-likes">1 Likes</span>
+                    <span className="user-post-info-dislikes">17 Dislikes</span>
+                    <span className="user-post-info-comments">15 Comments</span>
+                    <span className="user-post-info-views">502 Views</span>
+                </div>
+                <hr></hr>
+    
+                <div className="user-post-description">
+                    <span>{this.props.description}</span>
+                </div>
+            </div>)
+    }
+    
+};
 
 export default Post;
--- src/components/dashboard/Posts/Posts.js
+++ src/components/dashboard/Posts/Posts.js
@@ -1,11 +1,76 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './Posts.css';
 import Post from './Post/Post';
+import axios from 'axios';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
-const Posts = () => (
-    <div className="posts-container">
-        <Post />
-    </div>
-);
+class Posts extends Component {
+    constructor() {
+        super();
+    }
+
+    state = {
+        posts: [],
+        page: 0,
+        hasMore: true
+    }
+
+    // Fetch more posts from database according to page number
+    fetchMoreData = () => {
+        axios.get("/api/restricted-users/get-posts-with-page/?page=" + this.state.page).then(response => {
+            console.log(response);
+            console.log(response.data);
+            if (response.data.length > 0) {
+                this.setState({
+                    posts: this.state.posts.concat(response.data),
+                    page: this.state.page + 10
+                });
+                console.log("this is data " + this.state['posts'][0]);
+            } else {
+                this.setState({
+                    hasMore: false
+                });
+            }
+          });
+    };
+
+    componentDidMount() {
+        // Fetch initial number of posts after component render
+        this.fetchMoreData();
+    }
+
+    render() {
+        return (
+            <div className="posts-container">
+
+                 <InfiniteScroll
+                        height="100%"
+                        dataLength={this.state.posts.length}
+                        next={this.fetchMoreData}
+                        hasMore={this.state.hasMore}
+                        loader={<h4>Loading...</h4>}
+                        endMessage={
+                            <p style={{ textAlign: "center" }}>
+                            <b><br />You have seen it all !</b>
+                            </p>
+                        }
+                        >
+                        
+                        {/* Display fetched posts */}
+                         {this.state.posts.map((data, index) => (
+                            <Post   picture={data.picture}
+                                    audio={data.sound}
+                                    description={data.description}
+                                    audio_duration={data.audio_duration}
+                                    profile_picture={data.profile_picture}
+                                    user_name={data.user_name}
+                                    key={data._id}></Post>
+                        ))}
+
+                </InfiniteScroll>
+            </div>
+        );
+    }
+} 
 
 export default Posts;
